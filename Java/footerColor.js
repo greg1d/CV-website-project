af680a6cb884810b56f805afc415af5e9c563926
Greg Kudzin <gregorykudzin@gmail.com>
2024-11-16 16:09:53 -0500

    Added footer java script

---
+++ Java/footerColor.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+    const footer = document.querySelector('footer');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const footerColor = entry.target.getAttribute('data-footer-color');
+                console.log('data-footer-color:', footerColor); // Print the value of data-footer-color
+                footer.style.backgroundColor = footerColor;
+            }
+        });
+    }, { threshold: 0.5 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
